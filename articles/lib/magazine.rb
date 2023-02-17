38cc9cab1155c0c22f23ea094c18aacfe7c833bc
matomugo60 <matomugo60@gmail.com>
2023-02-17 13:40:29 +0300

    Added class Magazine to magazine.rb

--- articles/lib/magazine.rb
+++ articles/lib/magazine.rb
@@ -1,5 +1,13 @@
 class Magazine
-    
+    attr_accessor :name, :category
+    @@magazines = []
+
+    def initialize(name, category)
+        @name = name
+        @category = category
+        @@magazines << {:name => "#{name}", :category => "#{category}"}
+
+    end
 
 
 end
